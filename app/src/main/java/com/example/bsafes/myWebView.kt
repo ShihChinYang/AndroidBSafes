bed7eb5ac2eeaa8e159f96cc95060c2c2496c5cf
MacBook Pro - SYang <macbookpro-syang@MacBooks-MacBook-Pro.local>
2024-09-13 13:26:27 +0800

    Initial Setup

---
+++ app/src/main/java/com/example/bsafes/myWebView.kt
@@ -0,0 +1,122 @@
+package com.example.bsafes
+
+import android.content.Context
+import android.content.res.AssetManager
+import android.net.Uri
+import android.util.Log
+import android.webkit.ConsoleMessage
+import android.webkit.MimeTypeMap
+import android.webkit.ServiceWorkerClient
+import android.webkit.ServiceWorkerController
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.webkit.WebViewAssetLoader
+
+class myPathHandler(context: Context) : WebViewAssetLoader.PathHandler {
+    private val assetManager: AssetManager = context.getAssets()
+    override fun handle(path: String): WebResourceResponse {
+        val extension: String = MimeTypeMap.getFileExtensionFromUrl(path)
+        val mimeType: String?
+        if(extension == "js") {
+            mimeType = "text/javascript"
+        } else {
+            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+        }
+        try {
+            val response = WebResourceResponse(mimeType, "UTF-8", assetManager.open(path))
+            return response
+        } catch(e: Exception) {
+            return WebResourceResponse("text/html", "UTF-8", assetManager.open("404.html"))
+        }
+    }
+}
+const val PAGE_URL = "https://android.bsafes.com/logIn.html"
+
+@Composable
+fun WebViewScreen() {
+    var myFilePathCallback by remember { mutableStateOf<ValueCallback<Array<Uri>>?>(null) }
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.OpenMultipleDocuments() )
+    { selectedUri ->
+        myFilePathCallback?.onReceiveValue(selectedUri.toTypedArray())
+        myFilePathCallback = null
+    }
+    val myWebChromeClient = object: WebChromeClient() {
+        override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+            Log.d("WebView", consoleMessage.message())
+            return true
+        }
+
+        override fun onShowFileChooser(
+            webView: WebView?,
+            filePathCallback: ValueCallback<Array<Uri>>?,
+            fileChooserParams: FileChooserParams?
+        ): Boolean {
+            Log.d("WebView", fileChooserParams?.mode.toString())
+            Log.d("WebView", fileChooserParams?.acceptTypes.toString())
+
+            if(fileChooserParams?.acceptTypes?.isNotEmpty() == true) {
+                val mimeTypes =
+                    if (fileChooserParams.acceptTypes.size == 1 && fileChooserParams.acceptTypes[0] == ""
+                    ) arrayOf("*/*") else fileChooserParams.acceptTypes!!
+                myFilePathCallback = filePathCallback
+                launcher.launch(mimeTypes)
+                return true
+            } else return false
+        }
+    }
+    AndroidView(factory = { context ->
+        val assetLoader = WebViewAssetLoader.Builder()
+            .setDomain("android.bsafes.com")
+            .addPathHandler("/", myPathHandler(context))
+            .build()
+        val swController = ServiceWorkerController.getInstance()
+        val serviceWorkerClient = object: ServiceWorkerClient() {
+            override fun shouldInterceptRequest(request: WebResourceRequest): WebResourceResponse? {
+                return assetLoader.shouldInterceptRequest(request.url)
+            }
+        }
+        swController.setServiceWorkerClient(serviceWorkerClient)
+        val serviceWorkerWebSettings = swController.serviceWorkerWebSettings
+        serviceWorkerWebSettings.allowContentAccess = true
+        serviceWorkerWebSettings.allowFileAccess  = true
+
+        return@AndroidView WebView(context).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.allowFileAccess = true
+            settings.allowContentAccess = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.setSupportZoom(false)
+            webViewClient = object: WebViewClient() {
+                override fun shouldInterceptRequest(
+                    view: WebView?,
+                    request: WebResourceRequest
+                ): WebResourceResponse? {
+                    val interceptedWebResponse = assetLoader.shouldInterceptRequest(request.url)
+                    interceptedWebResponse?.let {
+                        if(request.url.toString().endsWith("js", true)) {
+                            it.mimeType = "text/javascript"
+                        }
+                    }
+                    return interceptedWebResponse
+                }
+            }
+            webChromeClient = myWebChromeClient
+        }
+    }, update = {
+            it.loadUrl(PAGE_URL)
+    })
+}
