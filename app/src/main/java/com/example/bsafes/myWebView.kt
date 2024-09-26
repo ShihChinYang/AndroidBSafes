78f45a3460b28e243d79fb5c2e9524f6e2d726ce
MacBook Pro - SYang <macbookpro-syang@MacBooks-MacBook-Pro.local>
2024-09-26 17:00:01 +0800

    Working on saving a file

--- app/src/main/java/com/example/bsafes/myWebView.kt
+++ app/src/main/java/com/example/bsafes/myWebView.kt
@@ -6,9 +6,11 @@ import android.content.Context
 import android.content.Intent
 import android.content.res.AssetManager
 import android.net.Uri
+import android.os.Build
 import android.util.Log
 import android.view.ViewGroup
 import android.webkit.ConsoleMessage
+import android.webkit.JavascriptInterface
 import android.webkit.MimeTypeMap
 import android.webkit.ServiceWorkerClient
 import android.webkit.ServiceWorkerController
@@ -18,6 +20,7 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -29,6 +32,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.webkit.WebViewAssetLoader
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 import kotlinx.coroutines.launch
 import java.net.URI
 
@@ -53,12 +59,12 @@ class myPathHandler(context: Context) : WebViewAssetLoader.PathHandler {
 const val LOCAL_HOST = "https://android.bsafes.com"
 const val PAGE_URL = LOCAL_HOST+ "/logIn.html"
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @SuppressLint("RestrictedApi")
 @Composable
 fun MyWebView() {
     var myFilePathCallback by remember { mutableStateOf<ValueCallback<Array<Uri>>?>(null) }
-    var requiredMimeType = remember { mutableStateOf<Array<String>>(arrayOf())}
+    val requiredMimeType = remember { mutableStateOf<Array<String>>(arrayOf())}
     var openBottomSheet by rememberSaveable { mutableStateOf(false) }
     val bottomSheetState = rememberModalBottomSheetState()
     val coroutineScope = rememberCoroutineScope()
@@ -95,6 +101,31 @@ fun MyWebView() {
             } else return false
         }
     }
+    val afterPermissionGrantAction = remember {mutableStateOf<(() -> Unit)?>(null)}
+    val writeExternalStoragePermission = rememberPermissionState(
+        permission = android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        onPermissionResult = {result->
+            if(result) {
+                afterPermissionGrantAction.value?.let {action -> action()}
+            }
+            afterPermissionGrantAction.value = null
+        }
+    )
+    class MyJavascriptInterface(private val context:Context) {
+        @JavascriptInterface
+        fun saveBinaryStringAsFile(string: String, fileName:String, type: String ) {
+            fun saveToFile() {
+                val stringLength = string.length
+                Toast.makeText(context, "string length: $stringLength", Toast.LENGTH_SHORT).show()
+            }
+            if(writeExternalStoragePermission.status.isGranted) {
+                saveToFile()
+            } else {
+                afterPermissionGrantAction.value = ::saveToFile
+                writeExternalStoragePermission.launchPermissionRequest()
+            }
+        }
+    }
     AndroidView(factory = { context ->
         val assetLoader = WebViewAssetLoader.Builder()
             .setDomain("android.bsafes.com")
@@ -111,6 +142,9 @@ fun MyWebView() {
         serviceWorkerWebSettings.allowContentAccess = true
         serviceWorkerWebSettings.allowFileAccess  = true
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
         return@AndroidView WebView(context).apply {
             layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
@@ -147,7 +181,7 @@ fun MyWebView() {
                             return false
                         }
                         if(targetDomain.equals("www.bsafes.com") || targetDomain.equals("v2.bsafes.com")) {
-                            val url = LOCAL_HOST + request?.url?.path
+                            val url = LOCAL_HOST + request.url?.path
                             view?.loadUrl(url)
                             return true
                         } else {
@@ -167,7 +201,29 @@ fun MyWebView() {
             webChromeClient = myWebChromeClient
         }
     }, update = {
-            it.loadUrl(PAGE_URL)
+        it.loadUrl(PAGE_URL)
+        it.addJavascriptInterface(MyJavascriptInterface(it.context), "Android")
+        if(writeExternalStoragePermission.status.isGranted) {
+            Log.d("WebView", "Permission is granted")
+        } else {
+            //afterPermissionGrantAction.value = ::saveToFile
+            Log.d("WebView", "Permission is not granted")
+            writeExternalStoragePermission.launchPermissionRequest()
+        }
+        /*
+            it.setDownloadListener({ url, userAgent, contentDisposition, mimeType, contentLength ->
+                val request = DownloadManager.Request(Uri.parse(url))
+                request.setMimeType(mimeType)
+                request.addRequestHeader("cookie", android.webkit.CookieManager.getInstance().getCookie(url))
+                request.addRequestHeader("User-Agent", userAgent)
+                request.setDescription("Downloading file...")
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                request.setDestinationInExternalFilesDir(it.context, Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType))
+                val dm = it.context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                dm.enqueue(request)
+                Toast.makeText(it.context, "Downloading File", Toast.LENGTH_LONG).show()
+            })*/
     })
     if(openBottomSheet) {
         MediaChooser(
